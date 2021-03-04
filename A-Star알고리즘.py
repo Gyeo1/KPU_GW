d79d9ea4d313e2065e8b95258e1144dd9dd956b4
Gyeo1 <rlatjdruf99@naver.com>
2021-03-05 00:15:39 +0900

    새로운 파이썬 파일 추가

---
+++ A-Star알고리즘.py
@@ -0,0 +1,102 @@
+class Node:
+    def __init__(self,parent=None,position=None):
+        self.parent=parent
+        self.position=position
+
+        self.g=0
+        self.h=0
+        self.f=0
+    def __eq__(self,other):
+        return self.position==other.position
+
+def heuristic(node, goal, D=1, D2=2 ** 0.5):  # Diagonal Distance로 휴리스틱 값을 구하는 함수!
+    dx = abs(node.position[0] - goal.position[0])
+    dy = abs(node.position[1] - goal.position[1])
+    return D * (dx + dy) + (D2 - 2 * D) * min(dx, dy)
+
+def aStar(maze,start,end):
+    startNode=Node(None,start)
+    endNode=Node(None,end)
+
+    openList=[]
+    closedList=[]
+
+    openList.append(startNode)
+
+    while openList:
+        currentNode=openList[0]
+        currentIdx=0
+
+        for index, item in enumerate(openList):
+            if item.f<currentNode.f:
+                currentNode=item
+                currentIdx=index
+        openList.pop(currentIdx)
+        closedList.append(currentNode)
+
+        if currentNode==endNode:
+            path=[]
+            current=currentNode
+            while current is not None:
+                path.append(current.position)
+                current=current.parent
+            return path[::-1]
+        children=[]
+        for newPosition in [(0,-1),(0,1),(-1,0),(1,0),(-1,-1),(-1,1),(1,-1),(1,1)]:
+            nodePosition=(
+                currentNode.position[0]+newPosition[0], #X축
+                currentNode.position[1]+newPosition[1] #Y축
+
+            )
+            within_range_criteria=[
+                nodePosition[0]>(len(maze)-1),
+                nodePosition[0]<0,
+                nodePosition[1]>(len(maze[len(maze)-1])-1),
+                nodePosition[1]<0,
+            ]
+
+            if any(within_range_criteria):
+                continue
+
+            if maze[nodePosition[0]][nodePosition[1]]!=0:
+                continue
+            new_node=Node(currentNode,nodePosition)
+            children.append(new_node)
+
+        for child in children:
+            if child in closedList:
+                continue
+            child.g=currentNode.g+1
+            child.h=((child.position[0]-endNode.position[0])**2)+((child.position[1]-endNode.position[1])**2)
+            child.f=child.g+child.h
+
+            if len([openNode for openNode in openList
+                    if child == openNode and child.g > openNode.g]) > 0:
+                continue
+
+            openList.append(child)
+
+def main():
+    # 1은 장애물
+    maze=   [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+    start = (2, 1)#(x축,y축)
+    end = (7,6)
+
+    path = aStar(maze, start, end)
+    print(path)
+    print("Hi my name is E")
+    print("다른사람이 추가한내용")
+    print("my name is Green")
+if __name__ == '__main__':
+    main()
+    #[(0, 0), (1, 1), (2, 2), (3, 3), (4, 3), (5, 4), (6, 5), (7, 6)]
